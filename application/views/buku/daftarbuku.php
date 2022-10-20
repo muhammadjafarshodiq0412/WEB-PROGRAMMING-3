93f16310fd029123ababa90061e68df09a018f9f
Jafar <mjafar.shodiq@myboost.co>
2022-10-20 20:40:30 +0700

    add comment at file daftarbuku.php

--- application/views/buku/daftarbuku.php
+++ application/views/buku/daftarbuku.php
@@ -24,6 +24,7 @@
                         </div>
                     </div> <?php } ?>
                 <!-- end looping -->
+                <!-- testing  -->
             </div>
         </div>
     </div>
